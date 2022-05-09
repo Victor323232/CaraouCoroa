aa790558f4e450cc487e6a2338ab713119f47801
Victor323232 <victorhugoabc59@gmail.com>
2022-05-09 15:38:33 -0300

    page resultado

---
+++ lib/resultado.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+
+class resultado extends StatefulWidget {
+  const resultado({Key? key}) : super(key: key);
+
+  @override
+  State<resultado> createState() => _resultadoState();
+}
+
+class _resultadoState extends State<resultado> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        child: Column(
+          children: <Widget>[
+
+          ],
+        ),
+      ),
+    );
+  }
+}
